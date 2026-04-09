9d0e47dff909f593a91ca12d592ab455de8bbffa
akodunveysel18-hash <akodunveysel18@gmail.com>
2026-04-09 22:21:03 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -39,7 +39,7 @@ export default function Home() {
       .eq("id", userId)
       .single();
 
-    setUserRole(data?.role || "chief");
+    setUserRole(data?.role || "yok");
   }
 
   async function loadData() {
