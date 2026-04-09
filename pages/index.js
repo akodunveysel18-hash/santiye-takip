e1c996547c858c8ca836665856d5b3da64e9ced6
akodunveysel18-hash <akodunveysel18@gmail.com>
2026-04-09 21:02:55 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,1 @@
-export default function Home() {
-  return (
-    <div style={{ padding: 40 }}>
-      <h1>TEST 999</h1>
-      <p>Bu yeni sayfa çalışıyorsa sorun çözüldü.</p>
-    </div>
-  );
-}
+<h1>TEST 999</h1>
